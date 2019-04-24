ed163064de7cd74009ef124968eb33b488e6c34c
CodewellAcademy <codewellacademy@codewells-imac-4.local>
2019-04-24 12:51:40 +0200

    Initial Commit

---
+++ NewMemoryGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewMemoryGame
+//
+//  Created by CodewellAcademy on 4/24/19.
+//  Copyright © 2019 CodewellAcademy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
